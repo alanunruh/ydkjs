880c7fe68b0852d386142eccca5566c957563ff8
Alan Unruh <alan.unruh@gmail.com>
2017-09-17 23:56:34 +0000

    Equality

---
+++ equality.js
@@ -0,0 +1,6 @@
+var a = "42";
+
+var b = 42;
+
+a == b;     // true, checks for value equality with coercion allowed
+a === b;    // false, checks for value equality without allowing coercion (strick equality)
